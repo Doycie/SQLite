77fc3bb77586ac1a09b1f29943d7a102e8f24949
Doycie <imcody97@gmail.com>
2018-05-16 17:10:01 +0200

    IDFQF trial

--- Program.cs
+++ Program.cs
@@ -8,16 +8,31 @@ namespace SQLite
 {
     class Program
     {
+        const string metadatastring = "metadata.sqlite";
+        const string cardatabasestring = "CarDatabase.sqlite";
+
 
         static void Main(string[] args)
         {
 
             SQLite db = new SQLite();
-            //db.BuildDatabase("CarDatabase.sqlite","autompg.sql");
 
-            db.ReadDatabase("CarDatabase.sqlite");
 
-  
+
+
+            //db.CreateDatabaseFile(metadatastring);
+             //db.MakeQFDictionary();
+             //db.FillMetaDBWithIDF(cardatabasestring,metadatastring,"brand");
+
+            // db.FillMetaDBWithIDF(cardatabasestring, metadatastring, "model");
+
+
+            
+            // db.ReadDatabase(metadatastring, "model");
+            db.ReadDatabase(metadatastring, "brand");
+
+            Console.ReadLine();
+
         }
        
     }
--- SQLite.cs
+++ SQLite.cs
@@ -13,23 +13,132 @@ namespace SQLite
 
         SQLiteConnection dbconnection;
         
-        public void ReadDatabase(string name)
+
+
+        public void ReadDatabase(string name, string attributeName)
         {
             OpenConnection(name);
 
-            string sql = "select * from autompg order by model_year desc";
+            string sql = "select * from "+ attributeName+" order by idfqf";
             SQLiteCommand command = new SQLiteCommand(sql, dbconnection);
             SQLiteDataReader reader = command.ExecuteReader();
             while (reader.Read())
-                Console.WriteLine("Name: " + reader["model"] + "\tYear: " + reader["model_year"]);
+                
+                Console.WriteLine(attributeName+": " + reader[attributeName] + "\tIDFQF: " + reader["idfqf"]);
 
-            Console.ReadLine();
+  
 
 
             CloseConnection();
         }
 
 
+        Dictionary<Tuple<string, string>, int> qfoccurrences = new Dictionary<Tuple<string, string>, int>();
+        int QFMax = 0;
+        public void MakeQFDictionary()
+        {
+
+            StreamReader sr = new StreamReader("workload.txt");
+            string input;
+
+            input = sr.ReadLine();
+            input = sr.ReadLine();
+            
+            while ((input = sr.ReadLine()) != null)
+            {
+                int position = 0;
+
+                string[] inputSplit = input.Split();
+                
+                int times = int.Parse(inputSplit[0]);
+
+                while(inputSplit[position] != "WHERE")
+                {
+                    position++;
+                }
+
+                while (position < inputSplit.Length- 1)
+                {
+
+                    var key = Tuple.Create(inputSplit[position + 1], inputSplit[position + 3]);
+
+                    if (qfoccurrences.ContainsKey(key))
+                    {
+                        qfoccurrences[key] += times;
+                    }
+                    else
+                    {
+                        qfoccurrences.Add(key, times);
+                    }
+
+                    position += 4;
+                }
+
+            }
+
+            int max = 0;
+
+            foreach (int value in qfoccurrences.Values)
+            {
+                if( value > max)
+                {
+                    max = value;
+                }
+            }
+
+            QFMax = max;
+         
+
+     
+        }
+
+        public void FillMetaDBWithIDF(string OriginalDatabase, string metaDatabase , string attributeName )
+        {
+
+
+
+            OpenConnection(OriginalDatabase);
+
+
+            double total = 0;
+            Dictionary<string, int> occurrences = new Dictionary<string, int>();
+
+            string sql = "select * from autompg";
+            SQLiteCommand command = new SQLiteCommand(sql, dbconnection);
+            SQLiteDataReader reader = command.ExecuteReader();
+            while (reader.Read()) {
+
+                
+                string m = reader[attributeName].ToString();
+                total++;
+                if (occurrences.ContainsKey(m))
+                    occurrences[m] += 1;
+                else
+                    occurrences.Add(m,1);
+
+            }
+
+            CloseConnection();
+
+            OpenConnection(metaDatabase);
+            
+            ExecuteCommand("CREATE TABLE " + attributeName  + " ("+ attributeName + " text, idfqf real)");
+
+            foreach ( KeyValuePair<string,int> vp in occurrences)
+            {
+                float qf = 1;
+                Tuple<string,string> key = Tuple.Create(attributeName, "'"+vp.Key+"'");
+                if (qfoccurrences.ContainsKey(key))
+                {
+                    qf = (float)(qfoccurrences[key]+1) / (float)(QFMax+1);
+                }
+           
+
+                ExecuteCommand("INSERT into " + attributeName+ " VALUES ( '"  + vp.Key + "'," + Math.Log( total/ vp.Value ) * qf + ");");
+            }
+
+            CloseConnection();
+        }
 
         public void BuildDatabase(string nameOfDatabase, string fileToLoadFrom)
         {
@@ -45,17 +154,7 @@ namespace SQLite
             CloseConnection();
         }
         
-        void CreateExampleTable()
-        {
-            string sql = "create table highscores (name varchar(20), score int)";
-            ExecuteCommand(sql);
-            sql = "insert into highscores (name, score) values ('Me', 3000)";
-            ExecuteCommand(sql);
-            sql = "insert into highscores (name, score) values ('Myself', 6000)";
-            ExecuteCommand(sql);
-            sql = "insert into highscores (name, score) values ('And I', 9001)";
-            ExecuteCommand(sql);
-        }
+
 
 
         void ExecuteCommand(string com)
@@ -64,7 +163,7 @@ namespace SQLite
             command.ExecuteNonQuery();
         }
 
-        void CreateDatabaseFile(string name)
+        public void CreateDatabaseFile(string name)
         {
             SQLiteConnection.CreateFile(name);
         }
